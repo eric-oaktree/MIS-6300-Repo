aa9414cc214a7ea5784987b3e340c3a9d68f76d9
inspectorbean <erkoak@gmail.com>
2017-09-15 16:50:51 -0400

    work

---
+++ test.py
@@ -0,0 +1,3 @@
+
+
+print('test %s' %'code ' * 10)
--- week_two.py
+++ week_two.py
@@ -1,4 +1,20 @@
-def fun_ction():
-    return "So much Fun!"
 
-print(fun_ction())
+class Obj():
+    def __init__(self, v):
+        self.test = 1
+        self.total = v
+    def add_1(self):
+        b = self.total
+        self.total = self.total + 1
+        print(("You had "+str(b)+", Now you have "+str(self.total)))
+    def fun_ction(self):
+        return "So much Fun!"
+
+
+o = Obj(-10)
+x = 0
+print(o.fun_ction())
+while o.total <= 9:
+    o.add_1()
+    x = x + 1
+print(o.fun_ction())
